3dcc8cadf75eedb3943748dbe87faa77c6ee9809
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2014-09-24 16:39:45 +0300

    Make basic User model (including secure password gem bcrypt-ruby)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+	#uncomment one and comment the second to enable secure_passwords of 'bcrypt-ruby' gem
+	has_secure_password
+    #attr_accessor :password, :password_confirmation         
+
+	before_save { self.email = email.downcase }
+	validates :name,  presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, 
+						format: { with: VALID_EMAIL_REGEX },
+                    	uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 6 }
+        
 end
