e840d64ffbba77ba804b6586c74f31f6a7bdea15
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2014-09-25 17:15:36 +0300

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,38 @@ class User < ActiveRecord::Base
     #attr_accessor :password, :password_confirmation         
 
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
+
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, 
 						format: { with: VALID_EMAIL_REGEX },
                     	uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6 }
-        
+
+
+    def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+    def User.new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end        
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924122052) do
+ActiveRecord::Schema.define(version: 20140925120943) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,8 +22,10 @@ ActiveRecord::Schema.define(version: 20140924122052) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
